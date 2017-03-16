1813723c1126ca1695a1354fb5b7da517ef9604d
Juho Vepsalainen <bebraw@gmail.com>
2017-03-16 13:38:22 +0200

    chore: Disable more rules

--- prose-retext.js
+++ prose-retext.js
@@ -3,11 +3,9 @@
 var unified = require('unified');
 var remark2retext = require('remark-retext');
 var english = require('retext-english');
-//var quotes = require('retext-quotes');
 var sentenceSpacing = require('retext-sentence-spacing');
 var repeated = require('retext-repeated-words');
 var indefinite = require('retext-indefinite-article');
-var contractions = require('retext-contractions');
 
 module.exports = attacher;
 
@@ -15,9 +13,7 @@ function attacher() {
   this.use(remark2retext, unified()
     .use(english)
     .use(sentenceSpacing, {preferred: 1})
-    //.use(quotes, {preferred: 'smart'})
     .use(repeated)
     .use(indefinite)
-    .use(contractions)
   );
 }
